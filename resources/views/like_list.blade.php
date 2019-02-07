f99ac71a6f515b70adf2b05c43e778d05b48ecf7
ShintaroNoguchi <ktmpj553@ybb.ne.jp>
2019-02-07 18:24:19 +0900

    いいねユーザ一覧機能追加

--- resources/views/like_list.blade.php
+++ resources/views/like_list.blade.php
@@ -5,11 +5,15 @@
 @endsection
 
 @section ('content')
-    <table id="list">
-        <tbody>
-            @foreach ($likes as $like)
-                <tr><td><a href="/profile?id={{ $like->getId() }}"><img src="{{ $like->getAvatarUrl() }}" alt="User Icon"></a></td><td><a href="/profile?id={{ $like->getId() }}">{{ $like->getName() }}</a></td></tr>
-            @endforeach
-        </tbody>
-    </table>
+    @if(count($likes) > 0)
+        <table id="list">
+            <tbody>
+                @foreach ($likes as $like)
+                    <tr><td><a href="/profile?id={{ $like->getId() }}"><img src="{{ $like->getAvatarUrl() }}" alt="User Icon"></a></td><td><a href="/profile?id={{ $like->getId() }}">{{ $like->getName() }}</a></td></tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>表示できるユーザがいません</p>
+    @endif
 @endsection
