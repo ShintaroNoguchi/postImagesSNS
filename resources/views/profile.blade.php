ca3e8dbacd54201b4add270885fa16639c59d3e1
ShintaroNoguchi <ktmpj553@ybb.ne.jp>
2019-02-09 14:29:09 +0900

    プロフィール画面のデザイン完成

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,32 @@
 @extends ('layouts.base')
 
+@section ('css')
+    <link href="/css/profile.css" rel="stylesheet" type="text/css">
+@endsection
+
 @section ('content')
-    <table id="userInfo">
-        <tbody>
-                <tr><td rowspan="2"><img src="{{ $avatar_url }}" alt="User Icon"></td><td>{{ $name }}</td></tr>
-                <tr><td>{{ $num_of_likes }}</td></tr>
-        </tbody>
-    </table>
-    <div id="images">
-        @foreach ($posts as $post)
-            <img src="data:image/png;base64,{{ $post['image'] }}" alt="image">
-        @endforeach
+    <div class="profile">
+        <div class="profileContainer">
+            <table id="userInfo">
+                <tbody>
+                        <tr><td rowspan="2" class="left"><img class="userIcon" src="{{ $avatar_url }}" alt="User Icon"></td><td class="right name">{{ $name }}</td></tr>
+                        <tr><td class="right numOfLikes">いいねの数：{{ $num_of_likes }}</td></tr>
+                </tbody>
+            </table>
+            <ul id="images">
+                @foreach ($posts as $post)
+                    <li>
+                        <div class="image">
+                            <img src="data:image/png;base64,{{ $post['image'] }}" alt="image">
+                        </div>
+                    </li>
+                    @if ($loop->last)
+                        @for ($i = 0; $i < 2 - ($loop->index % 3); $i++)
+                            <li><div class="image dummy"></div></li>
+                        @endfor
+                    @endif
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endsection
