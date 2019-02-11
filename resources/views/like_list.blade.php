ffba10f596c11106f5cd47c9e8994033a1feb2ac
ShintaroNoguchi <ktmpj553@ybb.ne.jp>
2019-02-11 13:03:59 +0900

    いいねリスト画面のデザイン完成

--- resources/views/like_list.blade.php
+++ resources/views/like_list.blade.php
@@ -1,19 +1,26 @@
 @extends ('layouts.base')
 
 @section ('css')
-    <link href="" rel="stylesheet" type="text/css">
+    <link href="/css/like_list.css" rel="stylesheet" type="text/css">
 @endsection
 
 @section ('content')
     @if(count($likes) > 0)
-        <table id="list">
-            <tbody>
-                @foreach ($likes as $like)
-                    <tr><td><a href="/profile?id={{ $like->getId() }}"><img src="{{ $like->getAvatarUrl() }}" alt="User Icon"></a></td><td><a href="/profile?id={{ $like->getId() }}">{{ $like->getName() }}</a></td></tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="likeList">
+            <div class="likeListContainer">
+                <table id="userInfo">
+                    <tbody>
+                        @foreach ($likes as $like)
+                            <tr>
+                                <td class="left"><a href="/profile?id={{ $like->getId() }}"><img class="userIcon" src="{{ $like->getAvatarUrl() }}" alt="User Icon"></a></td>
+                                <td class="right name"><a href="/profile?id={{ $like->getId() }}">{{ $like->getName() }}</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     @else
-        <p>表示できるユーザがいません</p>
+        <p id="none">表示できるユーザがいません</p>
     @endif
 @endsection
